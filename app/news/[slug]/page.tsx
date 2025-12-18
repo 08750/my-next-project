86e0b5590fbfa292963ef9647c0e6d21a5237a43
“08750” <Suzukihabatan2020@gmail.com>
2025-12-18 10:25:49 +0900

    ニュースにSSRを適用

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,32 +1,33 @@
 import Article from "@/app/_components/Article/Article";
 import { getNewsDetail } from "@/app/_libs/microcms";
-import styles from "./page.module.css"
+import styles from "./page.module.css";
 import ButtonLink from "@/app/_components/ButtonLink";
 import { notFound } from "next/navigation";
 
-type Props ={
-    params:{
-        slug:string;
-    };
-    searchParams:{
-        dk?:string;
-    }
+type Props = {
+  params: {
+    slug: string;
+  };
+  searchParams: {
+    dk?: string;
+  };
 };
 
-export default async function Page({ params , searchParams }: Props){
-    // return <div>{JSON.stringify(props)}</div>
-    const data = await getNewsDetail(params.slug , {
-        draftKey: searchParams.dk,
-    }).catch(notFound);
+export const revalidate = 0;
 
-    return(
-        // <div>{data.title}</div>
-        <>
-            <Article data={data}/>
-            <div className={styles.footer}>
-                <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
-            </div>
-        </>
-    )
+export default async function Page({ params, searchParams }: Props) {
+  // return <div>{JSON.stringify(props)}</div>
+  const data = await getNewsDetail(params.slug, {
+    draftKey: searchParams.dk,
+  }).catch(notFound);
 
+  return (
+    // <div>{data.title}</div>
+    <>
+      <Article data={data} />
+      <div className={styles.footer}>
+        <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
+      </div>
+    </>
+  );
 }
